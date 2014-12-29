aba9cd4fa5e57d957efeef4c62912d4d4ed81432
FranciellyM <smnarf@gmail.com>
2014-12-29 13:24:11 -0200

    Modelagem da homeActivity

--- app/src/main/java/triangulum/com/guiaa/HomeActivity.java
+++ app/src/main/java/triangulum/com/guiaa/HomeActivity.java
@@ -1,11 +1,12 @@
 package triangulum.com.guiaa;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +19,4 @@ public class HomeActivity extends ActionBarActivity {
         //Raissa
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
