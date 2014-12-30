13325ea68d540e641e7c3834f4cff8258a13b892
FranciellyM <smnarf@gmail.com>
2014-12-30 17:07:11 -0200

    Inicio da implementação da visualização de detalhe de um anunciante.

---
+++ app/src/main/java/triangulum/com/guiaa/fragment/DetalheLocalFragment.java
@@ -0,0 +1,22 @@
+package triangulum.com.guiaa.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import triangulum.com.guiaa.R;
+
+
+public class DetalheLocalFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detalhe_local, container, false);
+    }
+
+
+}
